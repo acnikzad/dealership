9935d54640c59007a41ded0642c6de01fcac80af
Aria Nikzad <a.c.nikzad@gmail.com>
2022-08-29 19:13:45 -0700

    added cars controller

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -1,2 +1,58 @@
 class Api::CarsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @cars = Car.all
+    render json: @cars
+  end 
+
+  def show
+    the_id = params[:id]
+    @car = Car.find_by(id: the_id)
+    render json: @car
+  end
+
+  def create
+    @car = Car.create(
+      year: params[:year],
+      make: params[:make],
+      model: params[:model],
+      color: params[:color],
+      drivetrain: params[:drivetrain],
+      type: params[:type],
+      price: params[:price],
+      mileage: params[:mileage],
+      dealership_id: params[:dealership_id],
+      )
+    @car.save
+    render :show
+  end
+
+  def update
+    the_id = params[:id]
+    @car = Car.find_by(id: the_id)
+    @car.year = params[:year] || @car.year
+    @car.make = params[:make] || @car.make
+    @car.model = params[:model] || @car.model
+    @car.color = params[:color] || @car.color
+    @car.drivetrain = params[:drivetrain] || @car.drivetrain
+    @car.type = params[:type] || @car.type
+    @car.price = params[:price] || @car.price
+    @car.mileage = params[:mileage] || @car.mileage
+    @car.dealership_id = params[:dealership_id] || @car.dealership_id
+
+    @car.save
+    render :show
+  end
+
+  def destroy
+    @car = Car.find_by(id: params[:id])
+
+    if @car.present?
+      @car.destroy
+      @car.save
+    end
+
+    render
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,31 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => "users#index"
+    get "/users/:id" => "users#show"
+    post "/users" => "users#create"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+
+    get "/dealerships" => "dealerships#index"
+    get "/dealerships/:id" => "dealerships#show"
+    post "/dealerships" => "dealerships#create"
+    patch "/dealerships/:id" => "dealerships#update"
+    delete "/dealerships/:id" => "dealerships#destroy"
+
+    get "/cars" => "cars#index"
+    get "/cars/:id" => "cars#show"
+    post "/cars" => "cars#create"
+    patch "/cars/:id" => "cars#update"
+    delete "/cars/:id" => "cars#destroy"
+
+    get "/requests" => "requests#index"
+    get "/requests/:id" => "requests#show"
+    post "/requests" => "requests#create"
+    patch "/requests/:id" => "requests#update"
+    delete "/requests/:id" => "requests#destroy"
+
+
+  end
 end
