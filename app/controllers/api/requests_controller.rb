3a42683de034e32cdbd53512bf59dfb1f53719fc
Aria Nikzad <a.c.nikzad@gmail.com>
2022-08-30 16:55:42 -0700

    finished other controllers

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -1,2 +1,56 @@
 class Api::RequestsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @requests = Request.all
+    render json: @requests
+  end 
+
+  def show
+    the_id = params[:id]
+    @request = Request.find_by(id: the_id)
+    render json: @request
+  end
+
+  def create
+    @request = Request.create(
+      year: params[:year],
+      make: params[:make],
+      model: params[:model],
+      color: params[:color],
+      drivetrain: params[:drivetrain],
+      type: params[:type],
+      user_id: params[:user_id],
+      dealership_id: params[:dealership_id],
+      )
+    @request.save
+    render :show
+  end
+
+  def update
+    the_id = params[:id]
+    @request = Request.find_by(id: the_id)
+    @request.year = params[:year] || @request.year
+    @request.make = params[:make] || @request.make
+    @request.model = params[:model] || @request.model
+    @request.color = params[:color] || @request.color
+    @request.drivetrain = params[:drivetrain] || @request.drivetrain
+    @request.type = params[:type] || @request.type
+    @request.user_id = params[:user_id] || @request.user_id
+    @request.dealership_id = params[:dealership_id] || @request.dealership_id
+
+    @request.save
+    render :show
+  end
+
+  def destroy
+    @request = Request.find_by(id: params[:id])
+
+    if @request.present?
+      @request.destroy
+      @request.save
+    end
+
+    render
+  end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,53 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @users = User.all
+    render json: @users
+  end 
+
+  def show
+    the_id = params[:id]
+    @user = User.find_by(id: the_id)
+    render json: @user
+  end
+
+  def create
+    @user = User.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_digest: params[:password_digest],
+      type: params[:type],
+      user_id: params[:user_id],
+      dealership_id: params[:dealership_id],
+      )
+    @user.save
+    render :show
+  end
+
+  def update
+    the_id = params[:id]
+    @user = User.find_by(id: the_id)
+    @user.first_name = params[:first_name] || @user.first_name
+    @user.last_name = params[:last_name] || @user.last_name
+    @user.email = params[:email] || @user.email
+    @user.password = params[:password] || @user.password
+    @user.password_digest = params[:password_digest] || @user.password_digest
+
+    @user.save
+    render :show
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+    if @user.present?
+      @user.destroy
+      @user.save
+    end
+
+    render
+  end
 end
